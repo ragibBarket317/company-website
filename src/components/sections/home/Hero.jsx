1b7a5ebcf4d1251bafea25d51d138b220a76edfe
Ragib Barket <ragibrahman86317@gmail.com>
2026-02-24 16:10:48 +0600

    problem fix

--- src/components/sections/home/Hero.jsx
+++ src/components/sections/home/Hero.jsx
@@ -56,7 +56,7 @@ const Hero = () => {
                   Engineering
                 </h1> */}
                 <div className="inline-block">
-                  <span className="flex gap-2 text-xs capitalize text-cyan-500 py-2 px-4 rounded-full bg-[#0A1428] border border-[#13203B]">
+                  <span className="flex gap-2 text-xs uppercase text-cyan-500 py-2 px-4 rounded-full bg-[#0A1428] border border-[#13203B]">
                     <Sparkles className="h-4 w-4" />
                     Enterprice grade delivery
                     <CircleSmall className="h-4 w-4 text-white" />
